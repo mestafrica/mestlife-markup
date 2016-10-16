55028998723fc0084befa3fa431f6124f9b76f67
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-10-16 22:52:01 +0000

    Added animation to status and mest-cards

--- assets/js/home-feed.js
+++ assets/js/home-feed.js
@@ -1,4 +1,5 @@
 $(document).on('ready', function(){
+	$('#feed-container').addClass('open');
 	$(".mitem").on('click', function(){
 		if ($(this).hasClass('is-active')) {return;}
 		$(".mitem").each(function(i){
